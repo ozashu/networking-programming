a5130f2ee291378dab8139015ccc859697f13919
(Shuhei Ozawa) <bm0801667@gmail.com>
2017-01-31 21:27:28 +0900

    networking-programming

--- networking programming/client.c
+++ networking programming/client.c
@@ -14,35 +14,36 @@ struct sockaddr_in server;
 int sock;
 char buf[32];
 int n;
-    
+
 /*ソケットの作成*/
 sock = socket(AF_INET, SOCK_STREAM, 0);
-    
+
 /* 接続先指定用構造体の準備*/
 server.sin_family = AF_INET;
 server.sin_port = htons(11111);
-server.sin_len = sizeof(server);
-    
+server.sin_len = sizeof(server); 
+
 /* 127.0.0.1はlocalhost */
 inet_pton(AF_INET, "127.0.0.1", &server.sin_addr);
-    
+
 /* sockaddr_in 構造体の長さを設定 */
 server.sin_len = sizeof(server);
-    
+
 /* サーバに接続 */
-if (connect(sock, (struct sockaddr *)&server, sizeof(server)) != 0) {
-    perror("connect");
-    return 1;
+if (connect(sock, (struct sockaddr * )&server, sizeof(server)) != 0) {
+   perror("connect");
+   return 1;
 }
-        
+
 /* サーバからデータを受信 */
 memset(buf, 0, sizeof(buf));
-n = sread(sock, buf, sizeof(buf));
-        
+n = read(sock, buf, sizeof(buf));
+
 printf("%d, %s\n", n, buf);
-        
+
 /* socketの終了 */
 close(sock);
-        
+
 return 0;
 }
+
